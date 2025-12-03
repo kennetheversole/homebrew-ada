c734b827261d6d530a11b97337b8356e2f1ec06e
Kenneth Eversole <kenneth@opscompanion.ai>
2025-12-03 09:28:34 -0600

    Update checksums for v0.1.0

--- Formula/ada.rb
+++ Formula/ada.rb
@@ -5,10 +5,10 @@ class Ada < Formula
 
   if Hardware::CPU.arm?
     url "https://github.com/kennetheversole/ada/releases/download/v0.1.0/ada-aarch64-apple-darwin.tar.gz"
-    sha256 ""
+    sha256 "7bd94533f9970ed2c5c58b64eef1becfa8c2ae34fb3d5b4cd1ed2f1b327db46f"
   else
     url "https://github.com/kennetheversole/ada/releases/download/v0.1.0/ada-x86_64-apple-darwin.tar.gz"
-    sha256 ""
+    sha256 "06c0fd47b2985a61eb1159d20487201fb816d146152762a321fc8e114dc0c5e3"
   end
 
   def install
