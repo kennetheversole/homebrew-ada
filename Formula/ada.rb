917d286f986982f058f4797b997278169425ea6d
Kenneth Eversole <kenneth@opscompanion.ai>
2025-12-03 10:09:48 -0600

    Fix checksums to match GitHub Actions build

--- Formula/ada.rb
+++ Formula/ada.rb
@@ -5,10 +5,10 @@ class Ada < Formula
 
   if Hardware::CPU.arm?
     url "https://github.com/kennetheversole/ada/releases/download/v0.1.1/ada-aarch64-apple-darwin.tar.gz"
-    sha256 "cf850f4e076dbcad53b351787c9a02a1d96448221267394c0d2d5e4e3e75688d"
+    sha256 "f7229d307817845118c76d2be2961712f178f7572d0930f5260c7242a8785ef0"
   else
     url "https://github.com/kennetheversole/ada/releases/download/v0.1.1/ada-x86_64-apple-darwin.tar.gz"
-    sha256 "a2ee2e4d7480ef1be8afb0ca3c0df45e8fd7c9679ae8d8c53c554f3d8f9bafc7"
+    sha256 "a71d3fbccad18bb2ad1a5e0201601719fba806c57d8e93167f851a25a8faed28"
   end
 
   def install
